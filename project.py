3008f335533b06b5ff3d7eb935a079293575aa70
Beata Morawska <40152995+BeataJoKo@users.noreply.github.com>
2024-08-15 01:45:03 +0200

    Update project.py
    class update with transformation

--- project.py
+++ project.py
@@ -85,12 +85,29 @@ moveImg(X_val, 'validation')
 moveImg(X_test, 'testing')
 
 #%%
-class ImgDataset(torch.utils.data.Dataset): 
-    def __init__(self, root_dir, data, labels, transform=None): 
+#%%
+class ImgDataset(Dataset): 
+    # Inicialization
+    def __init__(self, df_x, df_y, root_dir=None, transform=None): 
         self.root_dir = root_dir
-        self.images = list(data) 
-        self.labels = list(labels)
+        self.images = list(df_x) 
+        self.names = list(df_y)
         self.transform = transform 
+        self.labels = [1 if x == 'pneumonia' else 0 for x in self.names]
+        self.data = []
+        
+        if root_dir:
+            self.readImages()
+        
+    def readImages(self):
+        for i in range(0, len(self.images) - 1):
+            img_dir = os.path.join(self.root_dir, self.names[i])
+            image_path = os.path.join(img_dir, self.images[i]) 
+            image = Image.open(image_path)
+            # Applying the transformation 
+            if self.transform: 
+                image = self.transform(image)  
+            self.data.append(image)
   
     # Defining the length of the dataset 
     def __len__(self): 
@@ -98,69 +115,55 @@ class ImgDataset(torch.utils.data.Dataset):
   
     # Defining the method to get an item from the dataset 
     def __getitem__(self, index): 
-        img_dir = os.path.join(self.root_dir, self.labels[index])
-        image_path = os.path.join(img_dir, self.images[index]) 
-        image = np.array(Image.open(image_path)) 
-        label = self.labels[index]
-  
-        # Applying the transform 
-        if self.transform: 
-            image = self.transform(image) 
-          
+        image = self.data[index]
+        label = torch.tensor(self.labels[index])         
         return image, label
 
 #%%
 batch_size = 32
-transform = transforms.ToTensor()  
-train_dataset = ImgDataset('./data/training', X_train, y_train, transform)
-val_dataset = ImgDataset('./data/validation', X_val, y_val, transform)
-test_dataset = ImgDataset('./data/testing', X_test, y_test, transform)
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])  
 
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)  
-val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False) 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+train_dataset = ImgDataset(X_train, y_train, './data/training', transform)
+val_dataset = ImgDataset(X_val, y_val, './data/validation', transform)
+test_dataset = ImgDataset(X_test, y_test, './data/testing', transform)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)  
+val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False) 
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
 #%%
-print('Train: ', len(train_dataset))
-print('Image_0_name: ', train_dataset.images[0])
-print('Image_0_size: ', train_dataset[0][0].shape)
-print('Label_0: ', train_dataset.labels[0])
-print('Image_0_label: ', train_dataset[0][1])
+print('Test: ', len(test_dataset))
+print('Image_0_name: ', test_dataset.names[0])
+print('Image_0_size: ', test_dataset.data[0].shape)
+print('Image_0_label: ', test_dataset.labels[0])
+print('Object_0: ', test_dataset[0])
 
 #%%
 image_tensor, label = train_dataset[0]
 print(image_tensor.shape, label)
 
 #%%
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+print(example_data.shape)
+
+#%%
 for i in range(6):
-    plt.imshow(train_dataset[i][0][0], cmap='gray', interpolation='none')
-    plt.title("Ground Truth: {}".format(train_dataset[i][1]))
+    plt.imshow(train_dataset.data[i][0], cmap='gray', interpolation='none')
+    plt.title("Ground Truth: {}".format(train_dataset.names[i]))
     plt.xticks([])
     plt.yticks([])
     plt.show()
     
 #%%
+random_idx = random.randint(0, len(train_dataset) - 1) 
 
-
-
-
-
-
-
-#%%
-
-        
-#%%
-
-
-    
-#%%
-
-        
-        
-        
-        
-        
-        
-        
+plt.imshow(train_dataset.data[random_idx][0], cmap='gray', interpolation='none') 
+plt.xticks([])
+plt.yticks([])
+plt.show()
